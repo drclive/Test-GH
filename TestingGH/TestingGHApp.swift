ab5ef86b58d788887fbfc193b15f47355996fcf8
Clive Rudd <drclive@gmail.com>
2021-12-19 10:52:27 -0500

    Initial Commit

---
+++ TestingGH/TestingGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGHApp.swift
+//  TestingGH
+//
+//  Created by Clive Rudd Fernandez on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TestingGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
